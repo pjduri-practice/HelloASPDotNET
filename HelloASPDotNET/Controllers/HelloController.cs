6bd67ce9434dfa4759db4bffd45722d5bb052529
Sally Steuterman <sally.steuterman@gmail.com>
2023-02-09 19:30:25 -0600

    End of controllers and routing reading

---
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloASPDotNET.Controllers
+{
+    [Route("/helloworld")]
+    public class HelloController : Controller
+    {
+        // GET: /helloworld/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            string html = "<form method='post' action='/helloworld/welcome'>" +
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet Me!' />" +
+                "</form>";
+            return Content(html, "text/html");
+        }
+
+        // GET: /helloworld/welcome
+        // POST: /helloworld/welcome
+        [HttpPost("welcome")]
+        [HttpGet("welcome/{name?}")]
+        public IActionResult Welcome(string name = "World")
+        {
+            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+        }
+    }
+}
+
