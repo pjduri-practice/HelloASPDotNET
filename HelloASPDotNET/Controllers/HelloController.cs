3cc35f1a2ead55c5d5d5c5bf313073819c084513
Your Name <108307350+pjduri@users.noreply.github.com>
2023-03-19 22:51:31 -0500

    coded along to add basic controller

---
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloASPDotNET.Controllers
+{
+    [Route("/helloworld")]
+    public class HelloController : Controller
+    {
+        // GET:  /helloworld/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            string html = "<form method='post' action='/helloworld/welcome'>" +
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet Me!' />" +
+                "</form>";
+            return Content(html, "text/html");
+        }
+
+        // Get: /helloworld/welcome
+        // POST: /hello/welcome
+        [HttpPost("welcome")]
+        [HttpGet("welcome/{name?}")]
+        public IActionResult Welcome(string name = "World")
+        {
+            return Content($"<h1>Welcome to my app, {name}!</h1>", "text/html");
+        }
+    }
+}
